f2af0b488f32050deaaa803c67c2f86b629b6e24
benjamin stephan <benjamin@lafkon.net>
2023-12-06 12:20:42 +0100

    flexible meteodata and taup to log

--- taupunkt.ino
+++ taupunkt.ino
@@ -1,4 +1,10 @@
 #include "DHT.h"
+#include <RunningMedian.h>
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <ArduinoHttpClient.h>
+#include "conf.h"
 
 #define DHTPIN_0 2
 #define DHTPIN_I 4
@@ -10,13 +16,10 @@ DHT dht_I(DHTPIN_I, DHTTYPE);
 const int SWITCHPIN   = 12;
 const int MOSFETPIN_O =  9;
 const int MOSFETPIN_I = 10;
-const int analogInPin = 0; // TMP
+const int analogInPin =  0; // TMP
 
 // --- Internal ------------------------------------------------------
 int RUNMODE = 0;  //-1=OFF | 0=IDLE | 1=ENTFEUCHTUNG | 2=LUEFTUNG
-int lueftung_CountWaitIntervals = 0;
-int lueftung_CountRunIntervals = 0;
-int lueftung_Status = 0; //0=passive. 1=waiting. 2=running
 float humi_I;
 float humi_O;
 float temp_I;
@@ -24,6 +27,21 @@ float temp_O;
 float taup_I;
 float taup_O;
 float taup_delta;
+RunningMedian humi_I_MED = RunningMedian(19);
+RunningMedian humi_O_MED = RunningMedian(19);
+RunningMedian temp_I_MED = RunningMedian(19);
+RunningMedian temp_O_MED = RunningMedian(19);
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
+
+WiFiSSLClient wifi;
+int status = WL_IDLE_STATUS;
+HttpClient client = HttpClient(wifi, server, port);
+HttpClient wttr = HttpClient(wifi, server, port);
+
+boolean NOW;
+long lastTime;
 
 // --- FanValues -----------------------------------------------------
 String FANMODE;
@@ -33,26 +51,21 @@ float fan_speedNow   = 0.0; // REMEMBER TO CHECK
 float fan_O_speedNow = 0.0; // REMEMBER TO CHECK
 float fan_I_speedNow = 0.0; // REMEMBER TO CHECK
 
+String FAN_I,FAN_O;
+
 // --- WeatherValues -------------------------------------------------
 float temp_O_CORRECTION =  -3.0;
 float temp_I_CORRECTION =   0.0;
 
-float temp_I_MIN     =  10.0; // min. Temperatur Innen
+float temp_I_MIN     =  10.5; // min. Temperatur Innen
 float temp_O_MIN     = -10.0; // min. Temperatur Außen
-//float humi_MAX       =  62.0; // max. Luftfeuchte
+float humi_O_MAX     =  85.0; // max. Luftfeuchte Außen
 
 float taup_DIF       =   6.0; // minimaler Taupunktunterschied, bei dem das Relais schaltet
 float HYSTERESE      =   3.0; // Abstand von Ein- und Ausschaltpunkt
 
-// --- Timings ------------------------------------------------------
-float loop_Interval   =  2 *60; // Loop-/Messinterval in Sekunden
-
-int lueftung_WaitInterval = 60 *60;   // Lüftung alle x Sekunden (z.B. alle 60 Minuten)
-int lueftung_RunInterval  =  5 *60;   // für y Sekunden          (z.B.  für  5 Minuten)
-
 // --- Logging ------------------------------------------------------
 bool p = true;                        // Do serial print
-
 
 
 void setup() {
@@ -66,12 +79,31 @@ void setup() {
   pinMode(MOSFETPIN_I,OUTPUT);
 
   if(p) Serial.begin(9600);
+
+  while (status != WL_CONNECTED) {
+    if(p) Serial.print("Attempting to connect to Network named: ");
+    if(p) Serial.println(ssid);
+    status = WiFi.begin(ssid, pass);
+    delay(10000);
+  }
+  if(p) Serial.print("SSID: ");
+  if(p) Serial.println(WiFi.SSID());
+  IPAddress ip = WiFi.localIP();
+  IPAddress gateway = WiFi.gatewayIP();
+  if(p) Serial.print("IP Address: ");
+  if(p) Serial.println(ip);
+
+  timeClient.begin();
+  lastTime = WiFi.getTime();
+
 }
+
+void(* resetFunc) (void) = 0; // declare reset function @ address 0
 
 void loop() {
 
   // --- checkSwitch -------------------------------------------
-  if(digitalRead(SWITCHPIN) == HIGH) {
+  if ( digitalRead(SWITCHPIN) == HIGH ) {
     if(p && RUNMODE != -1) Serial.println("--- SILENT ON ------------------");
     RUNMODE = -1;
   } else {
@@ -79,145 +111,139 @@ void loop() {
     RUNMODE = 0;
   }
 
+  if ( RUNMODE != -1 ) {
+  
+    // --- check lastTime ----------------------------------------
+    if ( WiFi.getTime()-lastTime > 300 ) { // SET INTERVALL
+      NOW=true; 
+    } else { 
+      NOW=false; 
+    }
+    // -----------------------------------------------------------
 
-  if(RUNMODE != -1) {
-    
     // --- checkSensors -------------------------------------------    
-    // Reading temperature or humidity takes about 250 milliseconds!
-    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    humi_O = dht_O.readHumidity();
     humi_I = dht_I.readHumidity();
-    // Read temperature as Celsius (the default)
-    temp_O = dht_O.readTemperature() + temp_O_CORRECTION;
     temp_I = dht_I.readTemperature() + temp_I_CORRECTION;
+
+    humi_I_MED.add(humi_I);
+    temp_I_MED.add(temp_I);
+
+    humi_I = humi_I_MED.getMedian();
+    temp_I = temp_I_MED.getMedian();
+
+   if ( NOW == true ) {
+
+    int attempt = 0;
+    String response = "";
+    while ( response == "" && attempt < 10 ) {
+      wttr.get(meteorequest);
+      int statusCode = wttr.responseStatusCode();
+      response = wttr.responseBody();
+      //if(p) Serial.print("Response: ");Serial.println(response);
+      //if(p) Serial.print("Status code: ");Serial.println(statusCode);
+      int sep = response.indexOf(' ');
+      humi_O = stringToFloat(response.substring(sep+1));
+      temp_O = stringToFloat(response.substring(0, sep));
+      delay(5000);
+      wttr.stop();
+      attempt++;
+    }
+
+    if  ( isnan(humi_O) || isnan(temp_O) ) {
+        humi_O = dht_O.readHumidity();
+        temp_O = dht_O.readTemperature() + temp_O_CORRECTION;
+        humi_O_MED.add(humi_O);
+        temp_O_MED.add(temp_O);
+        humi_O = humi_O_MED.getMedian();
+        temp_O = temp_O_MED.getMedian();
+    }
 
     taup_O = taupunkt(temp_O,humi_O);
     taup_I = taupunkt(temp_I,humi_I);
     taup_delta = taup_I - taup_O;
-    
+
+    // --- collect postData ----------------------------------------------
+    String postData = String(temp_O)
+                    + ";"
+                    + String(temp_I)
+                    + ";"
+                    + String(humi_O)
+                    + ";"
+                    + String(humi_I)
+                    + ";"
+                    + String(taup_O)
+                    + ";"
+                    + String(taup_I);
+    // -------------------------------------------------------------------
+
+    timeClient.update();
+    //int H = timeClient.getHours();
+    int M = timeClient.getMinutes();
 
     // --- checkModeConditions -------------------------------------------
-    if ( (temp_I < temp_I_MIN ) || (temp_O < temp_O_MIN ) ) {                 // zu kalt
-      RUNMODE = 0;                                                            //   IDLE/WAIT (M0)
-    } else {                                                                  // nicht zu kalt
-      if (taup_delta >= taup_DIF) {                                           //   taup_delta passt
-        RUNMODE = 1;                                                          //     ENTFEUCHTUNG (M1)
-      } else if ( RUNMODE == 1 &&                                             
+    if ( (temp_I < temp_I_MIN) && (temp_O < temp_O_MIN) ) { // too cold
+      RUNMODE = 0;                                          //   IDLE/WAIT (M0)
+    } else {                                                // not too cold
+      if ( taup_delta >= taup_DIF ) {                       //   taup_delta ok
+        RUNMODE = 1;                                        //     ENTFEUCHTUNG (M1)
+      } else if (  RUNMODE == 1 &&                                             
                   (taup_delta < taup_DIF) && 
-                  (taup_delta > (taup_DIF - HYSTERESE)) ) {                   //   taup_delta passt immernoch
-        RUNMODE = 1;                                                          //     ENTFEUCHTUNG Keep running (M1)
-      } else {                                                                //   sonst
-        RUNMODE = 2;                                                          //     INTERVALLLUEFTUNG (M2)
-      } 
+                  (taup_delta > (taup_DIF - HYSTERESE)) ) { //   taup_delta still ok
+        RUNMODE = 1;                                        //     ENTFEUCHTUNG Keep running (M1)
+      } else {                                              //   otherwise
+        RUNMODE = 2;                                        //     INTERVALLLUEFTUNG (M2)
+      }
     }
-
-    //if (humi_O > humi_MAX+10 )                 RUN = false;                 // Könnte wieder rein
-
+    if ( (humi_O > humi_O_MAX) && RUNMODE == 1 ) RUNMODE = 2;  // Too wet outside
 
   // --- activateModes-------------------------------------------------------
     if ( RUNMODE == 0 ) {          // Switch or stay IDLE (M0)
-      fan(MOSFETPIN_I, 0.0);
-      fan(MOSFETPIN_O, 0.0); 
-      lueftung_Status = 0;
+      fan(MOSFETPIN_I, 0.0);FAN_I="0";
+      fan(MOSFETPIN_O, 0.0);FAN_O="0";
     } 
     else if ( RUNMODE == 1) {      // Switch or stay ENTFEUCHTUNG (M1)
-      fan(MOSFETPIN_I, 1.0);
-      fan(MOSFETPIN_O, 1.0);
-      lueftung_Status = 0;
+      fan(MOSFETPIN_I, 1.0);FAN_I="1";
+      fan(MOSFETPIN_O, 1.0);FAN_O="1";
     } 
-    else if (RUNMODE == 2) {       // Switch or stay LUEFTUNG (M2)
-      if(lueftung_Status == 0) {          //passive
-        lueftung_Status = 1;              
-        lueftung_CountWaitIntervals = 0;
-        lueftung_CountRunIntervals = 0;
-        fan(MOSFETPIN_I, 0.0);
-        fan(MOSFETPIN_O, 0.0); 
-      } 
-      else if (lueftung_Status == 1) {    //waiting
-        lueftung_CountWaitIntervals++;
-        if( (loop_Interval*lueftung_CountWaitIntervals) >= lueftung_WaitInterval ) {
-          lueftung_Status = 2;
-          lueftung_CountWaitIntervals = 0;
-        }
-        fan(MOSFETPIN_I, 0.0);
-        fan(MOSFETPIN_O, 0.0); 
-        if(p) Serial.print( "LUEFTUNG (M2) WAITING: "); 
-        if(p) Serial.print( (loop_Interval*lueftung_CountWaitIntervals)/60.0 ); 
-        if(p) Serial.println( " Minuten" );
-      } 
-      else if (lueftung_Status == 2) {    //runnning
-        lueftung_CountRunIntervals++;
-        if( (loop_Interval*lueftung_CountRunIntervals) >= lueftung_RunInterval ) {
-          lueftung_Status = 1;
-          lueftung_CountRunIntervals = 0;
-        }
-        fan(MOSFETPIN_I, 0.0);
-        fan(MOSFETPIN_O, 1.0); 
-        if(p) Serial.print( "LUEFTUNG (M2) RUNNING: "); 
-        if(p) Serial.print( (loop_Interval*lueftung_CountRunIntervals)/60.0 ); 
-        if(p) Serial.println( " Minuten" );
+    else if ( RUNMODE == 2 ) {       // Switch or stay LUEFTUNG (M2)
+      if ( M < 20 ) { // RUN FOR EVERY FIRST 20 MINUTES OF EACH HOUR
+        fan(MOSFETPIN_I, 0.0);FAN_I="0";
+        fan(MOSFETPIN_O, 1.0);FAN_O="1";
+      } else {
+        fan(MOSFETPIN_I, 0.0);FAN_I="0";
+        fan(MOSFETPIN_O, 0.0);FAN_O="0";
       }
     }
+    // ------------------------------------------------------------------------
+    String contentType = "application/x-www-form-urlencoded";
+    postData = String(RUNMODE)
+             + ";"
+             + postData
+             + ";"
+             + String(FAN_O)
+             + ";"
+             + String(FAN_I);
+  
+    // --- post to Server -----------------------------------------------------
+    if(p) Serial.println(postData);
+    postData = "dht=" + postData + "&token=" + token;
+    client.post("/dht/log.php", contentType, postData);
+    // show the status code and body of the response
+    int statusCode = client.responseStatusCode();
+    if(p) Serial.print("Status code: ");Serial.println(statusCode);
+    if ( statusCode != 200 ) resetFunc(); // call reset
+    delay(5000);
+    client.stop();
 
-    // else {
-    //     fan(MOSFETPIN_I, 0.0);
-    //     if ( humi_I < humi_MAX-5 ) {
-    //         fan(MOSFETPIN_O, 0.0);
-    //     } else if ( temp_I > temp_I_MIN &&
-    //                 humi_I < humi_MAX ) {
-    //         fan(MOSFETPIN_O, 1.0);
-    //     } else if ( humi_I > humi_MAX+2 ) {
-    //         fan(MOSFETPIN_O, 1.0);
-    //     } 
-    // }
-
-    //if ( temp_I < temp_I_MIN-4) { // EMERGENCY HALT
-    //     fan(MOSFETPIN_O, 0.0);
-    //     fan(MOSFETPIN_I, 0.0);
-    //}
-
-    if(p) Serial.print("RUNMODE: ");
-    if(p) Serial.println(RUNMODE);
-    if(p) Serial.print("taup_delta: ");
-    if(p) Serial.println(taup_delta);
-    if(p) Serial.println("------------");
-
-    // Serial.print("RUNMODE:");
-    // Serial.print(RUNMODE);
-    // Serial.print("|");
-    // Serial.print("humi_O:");
-    // Serial.print(humi_O);
-    // Serial.print("|");
-    // Serial.print("temp_O:");
-    // Serial.print(temp_O);
-    // Serial.print("|");
-    // Serial.print("taup_O:");
-    // Serial.print(taup_O);
-    // Serial.print("|");
-    // Serial.print("humi_I:");
-    // Serial.print(humi_I);
-    // Serial.print("|");
-    // Serial.print("temp_I:");
-    // Serial.print(temp_I);
-    // Serial.print("|");
-    // Serial.print("taup_I:");
-    // Serial.print(taup_I);
-    // Serial.print("|");
-    // Serial.print("RUN:");
-    // Serial.print(RUN);
-    // Serial.print("|");
-    // Serial.print("LIGHT:");
-    // Serial.print(light); // TMP
-    // Serial.println();
-
-    delay(loop_Interval*1000);    // Standard Loop-Interval
-
+    // --- remember Time  -----------------------------------------------------
+    lastTime = WiFi.getTime();
+   } else {
+     delay(5000);
+   }
   } else { // SILENT
-    lueftung_Status = 0;
     fan(MOSFETPIN_I, 0.0);
-    fan(MOSFETPIN_O, 0.0); 
+    fan(MOSFETPIN_O, 0.0);
     delay(1000);
   }
+
 } // END loop
-
-
